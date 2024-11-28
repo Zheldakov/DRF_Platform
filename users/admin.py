3268b2036855aed5475c1e7720f8bec3de15d81c
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-28 13:11:40 +0300

    Модуль User зарегистрирован в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from users.models import User
+# Регистрируем модель User в административной панели Django
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Отображение в админ панели пользователей"""
+    list_display = ('id','email','first_name','last_name', 'is_active',) # Показываем поля в списке модели
+    list_filter = ('id','email',) # Фильтрация
+    ordering = ('id',)
