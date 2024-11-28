ac9d2f9f8804d67ca2c2440e005e641c6ad426ad
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-28 14:55:40 +0300

    удален импорт, добавлен запрет кеширование для удаления

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.views.decorators.cache import never_cache
 
 from users.apps import UsersConfig
-from users.views import UserListAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView, UserRetrieveAPIView, \
-    UserTokenObtainParView
+from users.views import UserListAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView, UserRetrieveAPIView
 
 app_name = UsersConfig.name
 
@@ -14,7 +13,7 @@ urlpatterns = [
     path('create/', never_cache(UserCreateAPIView), name='user_create'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
     path('<int:pk>/update/', never_cache(UserUpdateAPIView.as_view()), name='user_update'),
-    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
+    path('<int:pk>/delete/', never_cache(UserDestroyAPIView.as_view()) , name='user_delete'),
 
     # token urlpatterns
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
