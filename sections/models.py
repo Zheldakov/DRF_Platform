11cb0d2ba648f23996634c1c4592590404c4b66f
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-12-01 17:54:15 +0300

    Написаны модели для разделов и их содержимого, проведена миграция

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+class Section(models.Model):
+    """Добавлена модель Раздел"""
+    title = models.CharField(max_length=150, verbose_name='Title')
+    description = models.TextField(verbose_name='Description', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Section'
+        verbose_name_plural = 'Sections'
+        ordering = ['id']
+
+class SectionContent(models.Model):
+
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name='Section')
+    title = models.CharField(max_length=150, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'SectionContent'
+        verbose_name_plural = 'SectionContent'
+        ordering = ['id']
