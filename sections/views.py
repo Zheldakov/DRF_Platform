1f31072c8bd2d6c0d1e2db6a1b51a1cdf6af9368
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-12-01 17:54:48 +0300

    Написаны endpoints для модели Раздел (Sections)

--- sections/views.py
+++ sections/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from sections.models import Section, SectionContent
+from sections.permissioms import IsModerator
+from sections.searializers.section_searializers import SectionListSerializer, SectionSerializer
+from sections.searializers.section_content_searializers import SectionContentListSerializer, SectionContentSerializer
+from sections.paginators import SectionPaginator, SectionContentPaginator
+
+
+class SectionListAPIView(ListAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = SectionPaginator
+
+
+class SectionCreateAPIView(CreateAPIView):
+    serializer_class = SectionSerializer
+    pagination_classes = [IsAuthenticated, IsAdminUser | IsModerator]
+
+
+class SectionRetrieveAPIView(RetrieveAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class SectionUpdateAPIView(UpdateAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated, IsAdminUser | IsModerator]
+
+class SectionDestroyAPIView(DestroyAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated, IsAdminUser | IsModerator]
